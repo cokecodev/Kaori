946d16050478862b2639f0f9f86ec2b7f2a99231
cokecode <you@example.com>
2023-03-14 01:54:38 +0800

    [update] user controller: 新增 me and logout、配合前端增加回傳之 data 、fix bug

--- controllers/user.js
+++ controllers/user.js
@@ -15,32 +15,38 @@ const userController = {
     if(!password || !username || !nickname) return res.json(errorMessage.missingError)
 
     try {
-      const isUsernameOccupied = await User.findOne({
+      let isUsernameOccupied = null
+      isUsernameOccupied = await User.findOne({
         where:{
           username
         }
       })
 
-      
       if(isUsernameOccupied !== null) return res.json(errorMessage.usernameOccupied)
       
       const hashPassword = bcrypt.hashSync(password, SALT_ROUNDS)
       const userData = await User.create({
         username,
         password: hashPassword,
-        nickname
+        nickname,
+        role:'normal' // 這裡如果不填，直接讓資料庫 default 的話，會拿不到 role ( 會收到 undefined )
       })
 
-      // 註冊完後,直接登入的機制。
       if(!userData) return res.json(errorMessage.internalServerError)
       req.session.username = username
       req.session.userId = userData.id
       req.session.role = userData.role
 
+      const data = {
+        username,
+        userId: userData.id,
+        role: userData.role,
+      }
+
       return res.status(200).json({
-        // TODO: 是否要直接回傳 userId ?
         ok: 1,
         message:'註冊成功',
+        data
       })
 
     } catch(err) {
@@ -49,7 +55,6 @@ const userController = {
     }
 
   },
-
   login: async (req, res) => {
     const { username, password } = req.body
     if(!password || !username) return res.json(errorMessage.missingError)
@@ -65,15 +70,20 @@ const userController = {
       const ifPasswordCorrect = await bcrypt.compare(password, userData.password)
       if(!ifPasswordCorrect) return res.json(errorMessage.loginFail)
 
-      // session
       req.session.username = username
       req.session.userId = userData.id
       req.session.role = userData.role
 
+      const data = {
+        username,
+        userId: userData.id,
+        role: userData.role,
+      }
+
       return res.status(200).json({
-        // TODO: 是否要直接回傳 userId ?
         ok: 1,
         message: '登入成功',
+        data
       })
 
     } catch(err) {
@@ -81,9 +91,31 @@ const userController = {
       return res.json(errorMessage.internalServerError)
     }
 
+  },
+  getMe: async (req, res) => {
+    const { username, userId, role } = req.session
+    if(!username || !userId || !role) return res.json(errorMessage.unauthorized)
+    
+    return res.status(200).json({
+      ok: 1,
+      message: '現為登入狀態!',
+      data:{
+        username,
+        userId,
+        role,
+      }
+    })
+  },
+  logout: async (req, res) => {
+    req.session.destroy(err =>{
+      if(err) return res.json(errorMessage.general)
+    })
+    
+    return res.status(200).json({
+      ok: 1,
+      message: '登出成功',
+    })
   }
-
-
 }
 
 module.exports = userController
